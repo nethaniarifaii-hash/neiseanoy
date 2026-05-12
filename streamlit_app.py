d8b957550c18611245ff92346f496248a2195c91
nethaniarifaii-hash <nethaniarifaii@gmail.com>
2026-05-12 09:02:29 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,13 +8,6 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-_LOREM_IPSUM = \"\"\"
-Lorem ipsum dolor sit amet, **consectetur adipiscing** elit, sed do eiusmod tempor
-incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-\"\"\"
-
-
 def stream_data():
     for word in _LOREM_IPSUM.split(" "):
         yield word + " "
